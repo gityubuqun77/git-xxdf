90a488453e700588183a369e63c381278f53d08c
song_wen_pan <song_wen_pan@163.com>
2021-12-10 09:12:02 +0800

    push commit

---
+++ src/main/java/com/swp/gitxxdf/GitXxdfApplication.java
@@ -0,0 +1,13 @@
+package com.swp.gitxxdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitXxdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitXxdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/gitxxdf/GitXxdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.gitxxdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXxdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
